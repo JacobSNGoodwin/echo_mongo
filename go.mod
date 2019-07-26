1efefdd3f1a279cbf6c7f0ece70ea887703553a3
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-25 17:15:53 -0700

    Add mongo db and graceful shutdown

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/Maxbrain0/echo_mongo
 
 go 1.12
 
-require github.com/labstack/echo/v4 v4.1.6
+require (
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/labstack/echo/v4 v4.1.6
+	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
+	github.com/xdg/stringprep v1.0.0 // indirect
+	go.mongodb.org/mongo-driver v1.0.4
+)
--- main.go
+++ main.go
@@ -1,24 +1,83 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/Maxbrain0/echo_mongo/model"
 	"github.com/labstack/echo/v4"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func main() {
+	// setup mongodB client
+	fmt.Println("Establishing connection to MongoDB...")
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully connected to MongoDB!")
+
+	// setup echo instance and routes
 	e := echo.New()
 	e.GET("/", helloWorld)
-	e.POST("/save", save)
-	e.Logger.Fatal(e.Start(":1323"))
+	e.POST("/tacos", postTaco)
+
+	// allows us to shut down server gracefully
+	go func() {
+		if err := e.Start(":1323"); err != nil {
+			e.Logger.Info("shutting down the server")
+		}
+	}()
+
+	// Wait for Control C to exit - shut down mongo and server
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+
+	// Block until a signal is received
+	<-quit
+
+	// shut down echo server
+	fmt.Println("Shutting down the echo server...")
+	ctx, cancel = context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+	fmt.Println("Successfully shut down echo server!")
+
+	// shut down mongo db
+	fmt.Println("Disconnecting from MongoDB...")
+
+	if err := client.Disconnect(ctx); err != nil {
+		log.Fatal("Problem shutting down mongodb")
+	}
+
+	fmt.Println("Succesfully Disconnect from MongoDB")
 }
 
 func helloWorld(c echo.Context) error {
 	return c.String(http.StatusOK, "Hello, World!")
 }
 
-func save(c echo.Context) error {
+func postTaco(c echo.Context) error {
 	meat := c.FormValue("meat")
 	description := c.FormValue("description")
 
