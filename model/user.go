7ce961cd61abb2dc978ad5254f791a1bd7a70109
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-07-29 13:28:55 -0700

    Allow form and xml request for user

--- model/user.go
+++ model/user.go
@@ -5,7 +5,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 // User contains data for tracking users
 type User struct {
 	ID       primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	UserName string             `json:"userName" bson:"userName"`
-	Email    string             `json:"email,omitempty" bson:"email,omitempty"`
-	Password string             `json:"password,omitempty" bson:"password, omitempty"`
+	UserName string             `json:"userName" xml:"userName" form:"userName" bson:"userName"`
+	Email    string             `json:"email,omitempty" xml:"email,omitempty" form:"email,omitempty" bson:"email,omitempty"`
+	Password string             `json:"password,omitempty" xml:"password,omitempty" form:"password,omitempty" bson:"password, omitempty"`
 }
